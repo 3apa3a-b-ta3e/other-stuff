ecbfac5d9319629122edcbd29c45a2fc2a0d7980
3apa3a-b-ta3e <3apa3a.b.ta3e@gmail.com>
2020-08-04 17:32:33 +0300

    Create yealink-phonebook.ps1
    This will export some data from Active Directory into special file for Yealink phones.
    Phones then can download it automatically.

---
+++ yealink-phonebook.ps1
@@ -0,0 +1,26 @@
+$FolderLocation = "C:\inetpub\wwwroot-dfsr\voip\"
+$strName = "company-phonebook"
+$stream = [System.IO.StreamWriter] "$FolderLocation\\$strName.xml"
+$stream.WriteLine("<YealinkIPPhoneDirectory>")
+
+$root = [ADSI]"LDAP://dc=company,dc=local"
+$filter = "(&(&(&(telephoneNumber=*)(!(msExchHideFromAddressLists=TRUE)))))"
+$props = 'name','telephoneNumber','mobile'
+$searcher = New-Object System.DirectoryServices.DirectorySearcher($root,$filter,$props)
+$searcher.Sort.PropertyName = "telephoneNumber"
+$searcher.Sort.Direction = [System.DirectoryServices.SortDirection]::Ascending
+$searcher.FindAll() | ForEach-Object{ 
+  $entry = $_.GetDirectoryEntry()
+  $name = $entry.name
+  $telephoneNumber = $entry.telephoneNumber
+  $mobile = $entry.mobile
+  $mobile = $mobile -replace "^."
+  $stream.WriteLine("  <DirectoryEntry>")
+  $stream.WriteLine("    <Name>"+ $name +"</Name>")
+  $stream.WriteLine("    <Telephone>"+ $telephoneNumber +"</Telephone>")
+  $stream.WriteLine("    <Telephone>"+ $mobile +"</Telephone>")
+  $stream.WriteLine("  </DirectoryEntry>")
+}
+
+$stream.WriteLine("</YealinkIPPhoneDirectory>")
+$stream.close()
